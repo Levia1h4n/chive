ff89efc2ba84e4dfdfcb979aa87d417db0e0a64a
sujin <cy_20200202@163.com>
2022-04-14 23:40:00 -0400

    login admin

--- chive-vue/src/router/index.js
+++ chive-vue/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history', // remove hash flag '#' in URL, Hash is a default vue-router mode setting
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex
     }
   ]
 })
