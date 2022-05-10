dec100ff9d2965939780239e621351c6d625ef10
yeri <pppyl831@outlook.com>
2022-05-09 21:18:29 -0400

    kchart

--- chive-vue/src/router/index.js
+++ chive-vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
+import Kchart from '@/components/home/Kchart'
 // import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -10,10 +11,10 @@ export default new Router({
   // mode: 'history', // remove hash flag '#' in URL, Hash is a default vue-router mode setting
   routes: [
     // {
-    //   path: '/',
+    //   path: '/hello',
     //   name: 'HelloWorld',
     //   component: HelloWorld
-    // }
+    // },
     {
       path: '/login',
       name: 'Login',
@@ -23,6 +24,11 @@ export default new Router({
       path: '/index',
       name: 'AppIndex',
       component: AppIndex
+    },
+    {
+      path: '/kchart',
+      name: 'Kchart',
+      component: Kchart
     }
   ]
 })
